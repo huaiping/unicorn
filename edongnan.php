8ae9c7ddc9c9cc5a3adb12a1901b6671a803f88e
huaiping <huaiping@live.cn>
2021-11-16 15:03:55 +0800

    updated

--- edongnan.php
+++ edongnan.php
@@ -1,4 +1,20 @@
 <?php
+/*
+CREATE DATABASE IF NOT EXISTS `exams` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+CREATE TABLE `edongnan` (
+  `xh` int(6) NOT NULL,
+  `xm` varchar(20) NOT NULL,
+  `bj` int(2) NOT NULL,
+  `zf` int(4) NOT NULL,
+  `bm` int(2) NOT NULL,
+  `jm` int(4) NOT NULL,
+  `kh` int(15) NOT NULL,
+  PRIMARY KEY (`xh`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+*/
+
+
 $db = new PDO('mysql:host=localhost; dbname=exams', 'root', 'xxxx');
 print("<table>");
 try {
