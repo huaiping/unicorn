0304b7363078721aca6347af0ac78193d0e94a65
Snoopy <huaiping@live.cn>
2023-03-12 09:15:58 +0800

    updated

--- edongnan.php
+++ edongnan.php
@@ -2,7 +2,7 @@
 /*
 CREATE DATABASE IF NOT EXISTS `exams` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE `edongnan` (
+CREATE TABLE `exams`.`edongnan` (
   `xh` varchar(6) NOT NULL,
   `xm` varchar(16) NOT NULL,
   `bj` varchar(2) NOT NULL,
@@ -20,7 +20,7 @@ try {
     $db = new PDO('mysql:host=localhost; dbname=exams', 'root', 'xxxx');
     for ($m = 0; $m < 60; $m++) {
         for ($i = 6; $i < 23; $i++) {
-            $query = $db->query("SELECT xh, xm, kh FROM edongnan WHERE bj=$i ORDER BY zf DESC, xh ASC LIMIT $m,1");
+            $query = $db->query("SELECT xh, xm, kh FROM edongnan WHERE bj=$i ORDER BY zf DESC, xh ASC LIMIT $m, 1");
             $row = $query->fetch();
             print_r("<tr><td>".$row['xh']."</td><td>".$row['kh']."</td><td>".$row['xm']."</td></tr>");
         }
